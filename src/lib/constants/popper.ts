68056aecd1ca6b2a9a2871507e6229024f661952
Hayden Woods <haydenw@genvis.co>
2022-06-12 14:52:29 +0800

    Minor import bug fix

--- src/lib/constants/popper.ts
+++ src/lib/constants/popper.ts
@@ -1,5 +1,6 @@
 import { Placement } from "@popperjs/core";
-import { Offset } from "../types/step";
+
+import { Offset } from "../types/popper";
 
 export const DEFAULT_PLACEMENT: Placement = "auto";
 export const DEFAULT_OFFSET: Offset = [0, 10];
